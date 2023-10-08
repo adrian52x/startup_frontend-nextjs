2abe15b326ecd651da027ea881add16ab7bf8d8f
Enachi Adrian <enachi.adrian52@gmail.com>
2023-10-08 16:33:11 +0200

    update

---
+++ app/actions/getMentors.js
@@ -0,0 +1,43 @@
+export default async function getMentors() {
+    try {
+    //   const {
+    //     category,
+    //     expertise,
+    //     location,
+    //     availability,
+    //   } = params;
+
+    //   let query = {
+    //     isMentor: true
+    //   };
+
+    //   if (category) {
+    //     query.category = category;
+    //   }
+
+    //   if (expertise) {
+    //     query.expertise = expertise;
+    //   }
+
+    //   if (location) {
+    //     query.location = location;
+    //   }
+
+    //   if (availability) {
+    //     query.availability = availability;
+    //   }
+
+      const mentors = await fetch('http://localhost:5000/api/mentors', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        //body: JSON.stringify(query)
+      }).then(res => res.json());
+
+      return mentors;
+
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,21 @@
-export default function Home() {
+import getMentors from "./actions/getMentors";
+import Container from "./components/Container";
+import MentorCard from "./components/mentors/MentorCard";
+
+export default async function Home() {
+
+  const mentors = await getMentors();
+
   return (
-    <div className="text-rose-500 text-2xl">Home page text</div>
+    <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
+            {mentors.map((mentor) => (
+                <MentorCard
+                    key={mentor._id}
+                    data={mentor}
+                />
+            ))}
+        </div>
+    </Container>
   )
 }
