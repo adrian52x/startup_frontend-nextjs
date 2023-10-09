2dc33e811f05e5a9ebad8d1c4cd0e0dead525e45
adrian52x <enachi.adrian52@gmail.com>
2023-10-09 16:40:47 +0200

    update

--- app/actions/getMentors.js
+++ app/actions/getMentors.js
@@ -27,14 +27,12 @@ export default async function getMentors() {
     //     query.availability = availability;
     //   }
 
-      const mentors = await fetch('http://localhost:5000/api/mentors', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        //body: JSON.stringify(query)
-      }).then(res => res.json());
+      
+      const mentors = await fetch('http://localhost:5000/api/mentors').then(res => res.json());
+      //const mentors = [response[3]];
 
+      // const response = await fetch('http://localhost:5000/api/mentors');
+      // const mentors = await response.json();
       
 
       return mentors;
