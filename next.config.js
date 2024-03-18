891bd6e136ae3ac880adf93e242a741d600bb099
Enachi Adrian <enachi.adrian52@gmail.com>
2024-03-19 00:48:39 +0100

    headers change for imgur.com

--- next.config.js
+++ next.config.js
@@ -5,7 +5,21 @@ const nextConfig = {
           'lh3.googleusercontent.com',
           'i.imgur.com'
         ]
-      }
+      },
+
+      async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: "default-src 'self'; img-src 'self' https://imgur.com;",
+              },
+            ],
+          },
+        ]
+      },
 }
 
 module.exports = nextConfig
